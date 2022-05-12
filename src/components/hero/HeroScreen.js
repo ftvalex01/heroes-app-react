9d6d857270564c1f8738d214b450776cdee9fdc1
alex <ftvalex@hotmail.com>
2022-05-12 17:14:08 +0200

    aplicando usememo

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,4 +1,4 @@
-import { useParams, Navigate } from "react-router-dom"
+import { useParams, Navigate, useNavigate } from "react-router-dom"
 import { getHeroById } from "../../helpers/getHeroById"
 
 
@@ -6,23 +6,56 @@ import { getHeroById } from "../../helpers/getHeroById"
 
 export const HeroScreen = () => {
 
+
   const {heroeId} = useParams()
   const hero = getHeroById(heroeId)
+  const navigate = useNavigate();
 
   if(!hero){
     return <Navigate to="/"/>
   }
 
-  const imagePath = `/assets/imagenes/${hero.id}.jpg`;
+  const handleReturn = () =>{
+      navigate(-1)
+  }
+
+
+  const {
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+  } = hero
+  const imagePath = `/assets/imagenes/${id}.jpg`;
 
   return (
     <div className="row mt-5">
         <div className="col-4">
           <img 
           src={imagePath} 
-          alt={hero.superhero}
+          alt={superhero}
           className="img-thumbnail"
           />
+        </div>
+        <div className="col-8">
+          <h3>{superhero}</h3>
+          <ul className="list-group">
+            <li className="list-group-item mb-2">
+              <b>Alter ego:</b> {alter_ego}    
+            </li>
+            <li className="list-group-item mb-2">
+              <b>Publisher:</b> {publisher}
+            </li>
+            <li className="list-group-item">
+              <b>first appearance:</b> {first_appearance}
+            </li>
+          </ul>
+          <h5 className="mt-4">characters</h5>
+          <p>{characters}</p>
+
+          <button className="btn btn-outline-info" onClick={handleReturn}>Atras</button>
         </div>
     </div>
   )
